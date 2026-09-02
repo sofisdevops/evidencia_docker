853b1f011f5994a2bbb1f6e5407d8adfcfa85d2b
sofia <sofis.devops@gmail.com>
2026-09-02 14:13:35 -0500

    Feat: Implementar Github Actions

---
+++ sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdirtempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/SOFIA/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/SOFIA/templates/" >> tempdir/Dockerfile
+echo "COPY ./sample.py /home/SOFIA/" >> tempdir/Dockerfile
+
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python3 /home/SOFIA/sample.py" >> tempdir/Dockerfile
