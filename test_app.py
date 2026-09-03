d3e4caa2dd0fbfe05aecec597a8b0bb7d3f3a326
sofia <sofis.devops@gmail.com>
2026-09-02 22:48:53 -0500

    Error: Pytest

--- test_app.py
+++ test_app.py
@@ -1,5 +1,18 @@
-def ejemplitoSencillo(a,b):
-    return a * b
+import importlib
+import pytest
 
-def test_del_ejemplitoSencillo ():
-    assert ejemplitoSencillo(6 , 8) == 48
+sample_app_module = importlib.import_module("sample-app")
+app = sample_app_module.app
+
+
+@pytest.fixture
+def client():
+  app.config["TESTING"] = True
+  with app.test_client() as client:
+    yield client
+
+
+def test_home_status_code(client):
+  """Prueba unitaria para verificar que la ruta principal responde 200 OK"""
+  response = client.get("/")
+  assert response.status_code == 502
